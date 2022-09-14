4e8add732b059b5b91471357cd91c28bcf26b89e
mathildadellanova <mathilda.dellanova@ui.ac.id>
2022-09-14 16:52:42 +0700

    menyelesaikan code

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # TODO: Create your views here.
+from katalog.models import CatalogItem
+
+# Create your views here.
+
+def show_katalog(request):
+    data_barang_wishlist = CatalogItem.objects.all()
+    context = {
+    'list': data_barang_wishlist,
+    'name': 'Mathilda',
+    'id' : '2106751240'
+    }
+    return render(request, "katalog.html",context)
